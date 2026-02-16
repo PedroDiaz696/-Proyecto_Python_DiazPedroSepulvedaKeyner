36eefa44d23bbe4545ece1e749048f66de7eba08
DiazPedro696 <pedrodiazz272@gmail.com>
2026-02-16 15:19:33 -0500

    avance...

--- Proyecto_Python_DiazPedroSepulvedaKeyner.py
+++ Proyecto_Python_DiazPedroSepulvedaKeyner.py
@@ -49,13 +49,20 @@ def menuInicioSesion():
     print("\n nombre o contraseña incorrectos!")
     print("")
      
-def menuTrainer():
+def menuTrainer(correo):
+    for trainer in trainers:
+        if trainer["correo"].strip().lower() == correo.strip().lower(): #si el correo que se ingreso es = al de un trainer:
+            trainer_actual = trainer #guarda la informacion del trainer para saber de quien se trata
+            break #sale del bucle apenas se cumpla
     print("--- BIENVENIDO TRAINER ---")
     print("1. Asignar notas")
     print("2. Mirar estudiantes de su grupo")
     opTrainer = int(input(": "))
     if opTrainer == 1:
-        asignarNotas()
+        asignarNotas(trainer_actual)
+    elif opTrainer == 2:
+        mirarestudiantesGrupotrainer(trainer_actual)
+
 
 
 modulos = [
@@ -110,7 +117,6 @@ def guardarNotas(registro):
 
         
 def menuCampers(correo):  #recibe el correo del camper 
-    camper_actual = None #indica que todavia no hay camper, es como cuando a una variable le ponemos = 0
     for camper in campers:
         if camper["correo"].strip().lower() == correo.strip().lower(): #si el correo que se ingreso es = al de un camper:
             camper_actual = camper #guarda la informacion del camper para saber de quien se trata
@@ -187,6 +193,155 @@ def menuCampers(correo):  #recibe el correo del camper
 
         elif opcionCamper == 4:
             print("Saliendo del menú camper...")
+            break
+
+
+def registrarExamenInicial():
+    
+        idBuscar = input("ID del camper: ")
+
+        for camper in campers:
+                if camper["id"] == idBuscar:
+                        teoria = float(input("Nota teorica: "))
+
+                        practica = float(input("Nota practica: "))
+
+                        promedio = (teoria + practica) / 2
+
+                        if promedio >= 60:
+                                camper["estado"] = "Aprobado"
+                                print("Camper aprobado")
+                        else:
+                                print("Camper no aprobado")
+                        return
+            
+
+        print("Camper no encontrado")
+
+def asignarRutaTrainer():
+    for r in rutas:
+        print("-", r["nombre"])
+
+    nombreRuta = input("Ruta: ")
+
+    for ruta in rutas:
+        if ruta["nombre"] == nombreRuta:
+
+            for t in trainers:
+                print("-", t["nombre"])
+            
+            nombreTrainer = input("Trainer: ")
+
+            ruta["trainer"] = nombreTrainer
+            print("Trainer asignado")
+            return
+
+def matricularCampus():
+    idBuscar = input("ID camper: ")
+
+    for camper in campers:
+
+        if camper["id"] == idBuscar:
+
+            if camper["estado"] == "Aprobado":
+                
+                print("El camper no está aprobado")
+
+                return
+
+            for ruta in rutas:
+                print("-", ruta["nombre"])
+
+            nombreRuta = input("Ruta: ")
+            
+            for ruta in rutas:
+
+                if ruta["nombre"] == nombreRuta:
+
+                    if len(ruta["campers"]) < ruta["capacidad"]:
+                        ruta["campers"].append(camper["id"])
+                        camper["ruta"] = nombreRuta
+                        camper["estado"] = "Cursando"
+
+                        matricula = {
+                                "camper": camper["id"],
+                                "ruta": nombreRuta,
+                                "trainer": ruta["trainer"],
+                                "fecha inicio": input("Fecha inicio: "),
+                                "fecha fin": input("Fecha fin: "),
+                                "salon": input("Salón: ")
+                            }
+                        matriculas.append(matricula)
+
+                        print("Camper matriculado")
+                        
+                else:
+
+                        print("Ruta llena")
+
+                return
+
+def evaluarModulo():
+
+    idBuscar = input("ID camper: ")
+
+    for camper in campers:
+
+        if camper["id"] == idBuscar:
+
+            teoria = float(input("Teoria: "))
+            practica = float(input("Práctica: "))
+            quiz = float(input("Quices: "))
+
+            notaFinal = teoria*0.3 + practica*0.6 + quiz*0.1
+
+            camper["notas"].append(notaFinal)
+
+            if notaFinal < 60:
+                camper["riesgo"] = True
+                print("Camper en riesgo")
+            else:
+                print("Módulo aprobado")
+            return
+
+def reporteRiesgo():
+
+    print("\n--- CAMPERS EN RIESGO ---")
+
+    for camper in campers:
+        if camper["riesgo"]:
+            print(camper["nombres"], camper["apellidos"])
+
+def menuCoordinador():
+
+    while True:
+
+        print("\n--- MENU COORDINADOR ---")
+        print("1. Registrar examen inicial")
+        print("2. Asignar trainer a ruta")
+        print("3. Matricular camper")
+        print("4. Evaluar módulo")
+        print("5. Ver campers en riesgo")
+        print("0. salir")
+        
+        op = input("Opción: ")
+
+        if op == "1":
+            registrarExamenInicial(campers)
+
+        elif op == "2":
+            asignarRutaTrainer(rutas, trainers)
+
+        elif op == "3":
+            matricularCamper(campers, rutas, matriculas)
+
+        elif op == "4":
+            evaluarModulo(campers)
+
+        elif op == "5":
+            reporteRiesgo(campers)
+
+        elif op == "0":
             break
 
 
